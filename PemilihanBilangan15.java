e5fce14a299f7e81b7781fb4bfec30a06b137ae0
Rifky <rifkypraditya1025@gmail.com>
2024-09-29 12:58:19 +0700

    modifikasi ternary operator

--- PemilihanBilangan15.java
+++ PemilihanBilangan15.java
@@ -7,11 +7,8 @@ public class PemilihanBilangan15 {
     System.out.print("Masukkan sebuah angka: ");
     int angka = sc.nextInt();
 
-    if (angka % 2 == 0) {
-      System.out.println("Angka " + angka + " termasuk bilangan genap");
-    }
-    else {
-      System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-    }
+    String hasil = (angka % 2 == 0) ? angka + " termasuk bilangan genap" : angka + " termasuk bilangan ganjil";
+
+    System.out.println(hasil);
   }
 }
